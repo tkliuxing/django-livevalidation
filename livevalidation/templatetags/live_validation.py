c7d381e4fce5d114f80f1baeb6081b833d74395c
dhan88 <dhan@cycle88.com>
2011-05-20 15:36:02 -0400

    Missed invalid hypen in var name for LV_EXTRA_SCRIPT rendering

--- livevalidation/templatetags/live_validation.py
+++ livevalidation/templatetags/live_validation.py
@@ -33,7 +33,7 @@ class ValidationNode(template.Node):
         for name,field in fields.items():
             result.append(self.do_field('%s%s'%(prefix,name),field))
         try:
-            result.append(LV_EXTRA_SCRIPT%{'fieldname':'id_%s'%fields.keys()[1]})
+            result.append(LV_EXTRA_SCRIPT%{'fieldname':'id_%s%s'%(prefix.replace('-', '_'),fields.keys()[1])})
         except:
             return ''
         result.append('</script>')
